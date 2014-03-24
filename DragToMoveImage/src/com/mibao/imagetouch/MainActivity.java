186a928272605d1a430c17ae8ee1f0318ea93d4a
zhuang0 <sheen.zhuang@gmail.com>
2014-03-24 23:12:19 +0800

    图片的自由拖动 freely to drag image

---
+++ DragToMoveImage/src/com/mibao/imagetouch/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mibao.imagetouch;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TouchImageView img = new TouchImageView(this);
+        setContentView(img);
+    }
+}
